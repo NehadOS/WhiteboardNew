8fad5a695b1e80e05b573d13a87945051b5aea4e
Nihade Osmani <A_nehado@hotmail.com>
2020-04-03 22:59:25 +0100

    Restored first file and fixed Manifest file

---
+++ app/src/main/java/com/example/whiteboardnew/CredentialsValidation.java
@@ -0,0 +1,52 @@
+package com.example.whiteboardnew;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class CredentialsValidation {
+    static Context ctx;
+
+    public CredentialsValidation(Context ct){
+        this.ctx = ct;
+    }
+
+    public static boolean emailValidation(String email) {
+        if (email.contains("@")) {
+            if (email.endsWith(".com") || email.endsWith(".uk")) {
+                return true;
+            } else {
+                Toast.makeText(ctx, "Wrong Domain, use .com or .uk", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        } else {
+            Toast.makeText(ctx, "Wrong Email Format", Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+
+
+
+
+    public static boolean passwordValidation(String pass, String confirmPass) {
+        if (pass.length() >= 6) {
+            if (pass.equals(confirmPass)) {
+                return true;
+            } else {
+                Toast.makeText(ctx, "Make Sure Passwords Match", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        } else {
+            Toast.makeText(ctx, "Password Must be At least 6 characters Long", Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+
+    public static boolean passwordValidation(String pass) {
+        if (pass.length() >= 6) {
+            return true;
+        } else {
+            Toast.makeText(ctx, "Password Must be At least 6 characters Long", Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+}
--- app/src/main/java/com/example/whiteboardnew/LoginActivity.java
+++ app/src/main/java/com/example/whiteboardnew/LoginActivity.java
@@ -1,37 +1,71 @@
 package com.example.whiteboardnew;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText loginUsername, loginPassword;
     private Button loginSubmit;
+    private TextView notAUserText;
 
-    private void initComponents(){
+    private void initComponents() {
         loginSubmit = findViewById(R.id.loginSubmit);
         loginUsername = findViewById(R.id.loginUsername);
         loginPassword = findViewById(R.id.loginPassword);
+        notAUserText = findViewById(R.id.notAUserText);
     }
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
         initComponents();
         loginSubmit.setOnClickListener(v -> {
-            new LoginClass(
-                    loginUsername.getText().toString(),
-                    loginPassword.getText().toString()).LogIn(this);
+            String username = loginUsername.getText().toString().isEmpty() ? "" : loginUsername.getText().toString();
+            String password = loginPassword.getText().toString().isEmpty() ? "" : loginPassword.getText().toString();
+            if (new CredentialsValidation(this).emailValidation(username)) {
+                if (new CredentialsValidation(this).passwordValidation(password)) {
+                    mAuth.signInWithEmailAndPassword(username, password)
+                            .addOnCompleteListener(LoginActivity.this, task -> {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+                                    startMainMenu(username.substring(0, username.indexOf("@")));
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                }
+            }
 
         });
 
+        notAUserText.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, SetUpProfileActivity.class);
+            startActivity(intent);
+        });
+    }
 
+    public void startMainMenu(String studentID) {
+        Intent intent = new Intent(LoginActivity.this, Main_menu.class);
+        intent.putExtra("studnetID", studentID);
+        startActivity(intent);
     }
 }
