fc418c783eb1326f5c9e7a4d82da59b976486443
Nihade Osmani <A_nehado@hotmail.com>
2020-04-03 22:35:26 +0100

    Restored files from backup, added LoginActivity

--- src/main/java/com/example/whiteboardnew/LoginActivity.java
+++ src/main/java/com/example/whiteboardnew/LoginActivity.java
@@ -3,12 +3,35 @@ package com.example.whiteboardnew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginUsername, loginPassword;
+    private Button loginSubmit;
+
+    private void initComponents(){
+        loginSubmit = findViewById(R.id.loginSubmit);
+        loginUsername = findViewById(R.id.loginUsername);
+        loginPassword = findViewById(R.id.loginPassword);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initComponents();
+        loginSubmit.setOnClickListener(v -> {
+            new LoginClass(
+                    loginUsername.getText().toString(),
+                    loginPassword.getText().toString()).LogIn(this);
+
+        });
+
+
     }
 }
